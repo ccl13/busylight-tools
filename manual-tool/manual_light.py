60378d33bf18c2e96d9bd907f8a982d00ac92850
Carlton Shen <1265370+ccl13@users.noreply.github.com>
2022-08-23 18:25:23 -0700

    Initial checkin.

---
+++ manual-tool/manual_light.py
@@ -0,0 +1,34 @@
+# PIP:
+# python3 -m pip install busylight-for-humans
+
+from busylight.lights.embrava import Blynclight
+
+light = Blynclight.first_light()
+
+color_red = (255, 0, 0)
+color_green = (0, 255, 0)
+color_blue = (0, 0, 255)
+color_yellow = (255, 255, 0)
+
+while True:
+    code = input('Enter command: ')
+    code = code.lower()
+
+    if code == "":
+        if light.color == color_green:
+            light.on(color_red)
+        else:
+            light.on(color_green)
+    elif code == "q":
+        light.off()
+        quit()
+    elif code == "r":
+        light.on(color_red)
+    elif code == "g":
+        light.on(color_green)
+    elif code == "b":
+        light.on(color_blue)
+    elif code == "y":
+        light.on(color_yellow)
+    elif code == "o":
+        light.off()
